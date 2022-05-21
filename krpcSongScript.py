f4f0c1ef9c44d6aeab14811de04cde968bdf517d
TimCatana <timothycatana@gmail.com>
2022-05-21 19:36:27 -0400

    working product

--- krpcSongScript.py
+++ krpcSongScript.py
@@ -7,17 +7,32 @@ for filename in os.listdir("./"):
   if filename != 'krpcSongScript.py' and filename != '.git':
     prs = Presentation(filename)
     for slide in prs.slides:
+      print("is slide")
       slide.background.fill.solid()
       slide.background.fill.fore_color.rgb = RGBColor(0, 0, 0)
 
       for shape in slide.shapes:
         if shape.shape_type == MSO_SHAPE_TYPE.TEXT_BOX:
+          print("is text box")
           if shape.has_text_frame:
-            print (shape.shape_type)
-            shape.text_frame.fit_text(font_family='Calibri', max_size=30, bold=False, italic=False, font_file=None)
+            print("has text frame")
+            shape.text_frame.fit_text(font_family='Calibri', max_size=50, bold=False, italic=False, font_file=None)
             for p in shape.text_frame.paragraphs:
-              p.font.color.rgb = RGBColor(255, 0, 0)
-          if shape.shape_type == MSO_SHAPE_TYPE.PICTURE:
-            shape.getparent().remove(shape._element)
-        # else if shape.shape_type 
-    # prs.save(filename.split('.')[0] + ' EDITED.pptx')
+              p.font.color.rgb = RGBColor(255, 255, 255)
+        
+        if shape.shape_type == MSO_SHAPE_TYPE.PLACEHOLDER:
+          print("is Placeholder")
+          if shape.has_text_frame:
+            print("placeholder has text frame")
+            shape.text_frame.fit_text(font_family='Calibri', max_size=50, bold=False, italic=False, font_file=None)
+            for p in shape.text_frame.paragraphs:
+              p.font.color.rgb = RGBColor(255, 255, 255)
+
+          else: 
+            print("has no text frame")
+
+        if shape.shape_type == MSO_SHAPE_TYPE.PICTURE:
+          print("PICTUREEEEEEEEEEEEEEEE")
+          shape._element.getparent().remove(shape._element)
+
+    prs.save(filename.split('.')[0] + ' EDITED.pptx')
