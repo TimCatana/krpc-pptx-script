f5ebc012a03a3831c078c5a6b31f211999e281e7
TimCatana <timothycatana@gmail.com>
2022-05-21 20:35:16 -0400

    Finished Version 1.1

--- krpcSongScript.py
+++ krpcSongScript.py
@@ -15,8 +15,35 @@ print("\nPlease make sure that no instances of powerpoint are open before you be
 for filename in os.listdir("./"):
 
   if filename != 'krpcSongScript.py' and filename != '.git' and filename != 'edited':
-    fontSize = input("enter desired font size: ")
     
+    # Font size
+    fontSize = raw_input("enter desired font size: ")
+    
+    # Font Family
+    fontFamily = raw_input("enter desired font (Calibri, etc..) if you input this correctly, the program will crash THIS IS CASE SENSITIVE. MOST FONTS CAPITALIZE THE FIRST LETTER: ")    
+    
+    # Font Bold
+    fontBold = raw_input("bold? y or n: ")
+    
+    while (str(fontBold) != 'y' and str(fontBold) != 'n'):
+      fontBold = raw_input("please enter a valid value - bold? y or n : ")
+      
+    if(str(fontBold) == 'y'):
+      fontBold = True 
+    else:
+      fontBold = False
+
+    # Font Italic
+    fontItalic = raw_input("italic? y or n: ")
+
+    while (str(fontItalic) != 'y' and str(fontItalic) != 'n'):
+      fontItalic = raw_input("please enter a valid value - italic? y or n : ")
+
+    if(str(fontItalic) == 'y'):
+      fontItalic = True 
+    else:
+      fontItalic = False
+
     try:
       prs = Presentation(filename)
     except BaseException as error:
@@ -29,13 +56,13 @@ for filename in os.listdir("./"):
       for shape in slide.shapes:
         if shape.shape_type == MSO_SHAPE_TYPE.TEXT_BOX:
           if shape.has_text_frame:
-            shape.text_frame.fit_text(font_family='Calibri', max_size=int(fontSize), bold=False, italic=False, font_file=None)
+            shape.text_frame.fit_text(font_family=str(fontFamily), max_size=int(fontSize), bold=fontBold, italic=fontItalic, font_file=None)
             for p in shape.text_frame.paragraphs:
               p.font.color.rgb = RGBColor(255, 255, 255)
         
         if shape.shape_type == MSO_SHAPE_TYPE.PLACEHOLDER:
           if shape.has_text_frame:
-            shape.text_frame.fit_text(font_family='Calibri', max_size=int(fontSize), bold=False, italic=False, font_file=None)
+            shape.text_frame.fit_text(font_family=str(fontFamily), max_size=int(fontSize), bold=fontBold, italic=fontItalic, font_file=None)
             for p in shape.text_frame.paragraphs:
               p.font.color.rgb = RGBColor(255, 255, 255)
 
