2e95f3200799714a043d849ce4f9caf26c09237b
TimCatana <timothycatana@gmail.com>
2022-05-21 20:17:37 -0400

    Finished product

--- krpcSongScript.py
+++ krpcSongScript.py
@@ -1,12 +1,26 @@
 import os
+import sys
 from pptx import Presentation
 from pptx.dml.color import RGBColor
 from pptx.enum.shapes import MSO_SHAPE_TYPE
 
+try:
+  os.mkdir('edited')
+  print("edited directory created, continuing...")
+except OSError as error:
+  print("edited directory already exists, continuing...")
+
+print("\nPlease make sure that no instances of powerpoint are open before you begin!!!\n")
+
 for filename in os.listdir("./"):
 
-  if filename != 'krpcSongScript.py' and filename != '.git':
-    prs = Presentation(filename)
+  if filename != 'krpcSongScript.py' and filename != '.git' and filename != 'edited':
+    fontSize = input("enter desired font size: ")
+    
+    try:
+      prs = Presentation(filename)
+    except BaseException as error:
+      print("Failed to open powerpoint file: " + filename) 
 
     for slide in prs.slides:
       slide.background.fill.solid()
@@ -15,17 +29,21 @@ for filename in os.listdir("./"):
       for shape in slide.shapes:
         if shape.shape_type == MSO_SHAPE_TYPE.TEXT_BOX:
           if shape.has_text_frame:
-            shape.text_frame.fit_text(font_family='Calibri', max_size=50, bold=False, italic=False, font_file=None)
+            shape.text_frame.fit_text(font_family='Calibri', max_size=int(fontSize), bold=False, italic=False, font_file=None)
             for p in shape.text_frame.paragraphs:
               p.font.color.rgb = RGBColor(255, 255, 255)
         
         if shape.shape_type == MSO_SHAPE_TYPE.PLACEHOLDER:
           if shape.has_text_frame:
-            shape.text_frame.fit_text(font_family='Calibri', max_size=50, bold=False, italic=False, font_file=None)
+            shape.text_frame.fit_text(font_family='Calibri', max_size=int(fontSize), bold=False, italic=False, font_file=None)
             for p in shape.text_frame.paragraphs:
               p.font.color.rgb = RGBColor(255, 255, 255)
 
         if shape.shape_type == MSO_SHAPE_TYPE.PICTURE:
           shape._element.getparent().remove(shape._element)
 
-    prs.save(filename.split('.')[0] + ' EDITED.pptx')
+      try:
+        prs.save('edited/' + filename.split('.')[0] + '.pptx')
+      except BaseException as error:
+        print('Failed to save file: ' + '\'edited/' + filename.split('.')[0] + '.pptx\' ' + 'please make sure that all instances of powerpoint are closed') 
+        sys.exit()
